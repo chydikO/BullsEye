369b7d20d0596f484bfc2c3f059e75ec1ec29e46
chydikO <oleg.chudnovskiy@gmail.com>
2019-12-04 22:22:15 +0200

    Less_4 Alert

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by maxxwell131 on 12/4/19.
+//  Copyright © 2019 maxxwell131. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello Word!!", message: "This is my first App !", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+}
+
