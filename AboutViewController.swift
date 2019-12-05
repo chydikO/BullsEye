ee9bd5a1283597d2e10497f7e57a040c107eef72
chydikO <oleg.chudnovskiy@gmail.com>
2019-12-05 19:04:22 +0200

    5.5 Adding Extra Screens

---
+++ AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by maxxwell131 on 12/5/19.
+//  Copyright © 2019 maxxwell131. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
