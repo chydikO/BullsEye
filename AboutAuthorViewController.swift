394b9f6db9f98e791502dcf7ac4a477a1fd31f09
chydikO <oleg.chudnovskiy@gmail.com>
2019-12-05 19:30:38 +0200

    5.6 Challenge Adding Extra Screens

---
+++ AboutAuthorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by maxxwell131 on 12/5/19.
+//  Copyright © 2019 maxxwell131. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
